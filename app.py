a644cd204bf2032dd9a9bfd85033cbdf583010bb
shafiq khuidad <shafiqkhuidad@gmail.com>
2019-10-20 21:23:20 +0500

    Tuples and unpacking in tuples

--- app.py
+++ app.py
@@ -1,7 +1,3 @@
-numbers = [ 3,5,70,2,1,9,10,50,60]
-max = numbers[0]
-for x in numbers:
-    if x > max:
-        max = x
-
-print(f'Max Number in the list of {numbers} is {max}')
+numbers = (10,20,30)
+x, y, z = numbers
+print(f' x= {x} , y= {y} , z= {z}')
