6e8eb55d3032195f11d5c0f2c6c9ca633e78e29f
shafiq khuidad <shafiqkhuidad@gmail.com>
2019-10-20 20:46:36 +0500

    Find Maximum number in the list of numbers

--- app.py
+++ app.py
@@ -1,13 +1,7 @@
-for x in range(4):
-    for y in range(3):
-        print(f'({x}, {y})')
-numbers = [5,2,5,2,2]
-
+numbers = [ 3,5,70,2,1,9,10,50,60]
+max = numbers[0]
 for x in numbers:
-    print('x' * x)
+    if x > max:
+        max = x
 
-for x in numbers:
-    output = ''
-    for y in range(x):
-        output +='x'
-    print(output)
+print(f'Max Number in the list of {numbers} is {max}')
