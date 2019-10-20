c656c61dc0fd4d0908cd35257c4ab857b9f3a365
shafiq khuidad <shafiqkhuidad@gmail.com>
2019-10-20 18:58:46 +0500

    Conitional Statments and Comparision operator

--- app.py
+++ app.py
@@ -1,14 +1,4 @@
-is_hot = False
-
-if is_hot:
-    print('its hot "day" today')
-else: 
-    print("Today is normal day")
-
-first = input('Enter your first Name ')
-last  = input('Enter your last Name ')
-
-if len(first) <=3 or len(last) <=3:
-    print("Name should be greater than three character")
-else: 
-    print(f"Hello {first} , {last} ")
+i = 1
+while i<=10:
+    print(f' {i} \n')
+    i = i + 1
