289dcc6e5388ccfe6f4c3a75e8f0b068667aa2aa
shafiq khuidad <shafiqkhuidad@gmail.com>
2019-10-20 18:54:42 +0500

    Operations on string

--- app.py
+++ app.py
@@ -2,5 +2,10 @@ is_hot = False
 
 if is_hot:
     print('its hot "day" today')
-else 
+else: 
     print("Today is normal day")
+
+first = input('Enter your first Name ')
+last  = input('Enter your last Name ')
+
+print(f"Hello {first} , {last} ")
