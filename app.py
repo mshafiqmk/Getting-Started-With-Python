b7e120d369cfd2b680eca427a326e86f32fda1f5
shafiq khuidad <shafiqkhuidad@gmail.com>
2019-10-20 18:56:34 +0500

    Operations on string

--- app.py
+++ app.py
@@ -8,4 +8,7 @@ else:
 first = input('Enter your first Name ')
 last  = input('Enter your last Name ')
 
-print(f"Hello {first} , {last} ")
+if len(first) <=3 or len(last) <=3:
+    print("Name should be greater than three character")
+else: 
+    print(f"Hello {first} , {last} ")
