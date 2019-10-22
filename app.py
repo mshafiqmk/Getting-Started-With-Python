f73631b7fd946b2ac0754c3e9055a27aa4b63bd7
shafiq khuidad <shafiqkhuidad@gmail.com>
2019-10-23 00:01:03 +0500

    Multiple Exception

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 
 try:
     age = int(input('Age : '))
+    income = 1000
+    risk = income / age 
+    print(age)
 except ValueError as error:
-    print(f'invalid value message => {error} ')
+    print(f'invalid value ErrorMessage => {error} ')
+except ZeroDivisionError :
+    print(f'Age cannot be zero ErrorMessage =>{ZeroDivisionError}')
