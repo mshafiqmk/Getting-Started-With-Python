e724504d781a31a10dbd19863dbb1105ba4305e3
shafiq khuidad <shafiqkhuidad@gmail.com>
2019-10-22 23:41:47 +0500

    Function and keyword arguments

--- app.py
+++ app.py
@@ -1,9 +1,6 @@
-phone = input("Phone: ")
-digits_mapping = {
-    "1" : "ONE",
-    "2" : "TWO",
-    "3" : "THREE",
-    "4" : "FOUR"
-}
-for ch in phone:
-    print(digits_mapping.get(ch,"!"))
+def greetUser(first,last):
+    print(f'hi {first} , {last}')
+
+greetUser('Muhammad','shafiq')
+# Function call with keyword arguments
+greetUser(last = 'adnan', first = 'Muhammad')
