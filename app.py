2e019a1f8da8669b88e32b05dc6272adca4dfaf3
shafiq khuidad <shafiqkhuidad@gmail.com>
2019-10-23 00:14:56 +0500

    Classes Exersise - person class

--- app.py
+++ app.py
@@ -1,16 +1,12 @@
-class Point :
-    def __init__(self,x,y) :
-        self.x = x
-        self.y = y
-    def move(self):
-        print("move")
-
-    def draw(self):
-        print("draw")
+class Person : 
+    def __init__(self,name):
+        self.name = name
+    def talk(self):
+        print(f'Hi , I am {self.name} ... ')
 
 
+shafiq = Person('Shafiq')
+shafiq.talk()
 
-point1 = Point(10,20)
-print(point1.x)
-point1.draw()
-
+adnan = Person('Adnan')
+adnan.talk()
