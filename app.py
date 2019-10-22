91adc93ca77da1d82a4cc172eb6982558b251e6c
shafiq khuidad <shafiqkhuidad@gmail.com>
2019-10-22 23:56:25 +0500

    Exception handling

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
-def greetUser(first,last):
-    print(f'hi {first} , {last}')
 
-greetUser('Muhammad','shafiq')
-# Function call with keyword arguments
-greetUser(last = 'adnan', first = 'Muhammad')
+try:
+    age = int(input('Age : '))
+except ValueError as error:
+    print(f'invalid value message => {error} ')
